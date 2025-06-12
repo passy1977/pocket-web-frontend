1d636e88160e4f65c3b166e3c16e4ad593a15a0e
Antonio Salsi <34831922+passy1977@users.noreply.github.com>
2025-06-12 23:35:46 +0200

    feat: start develop registration

--- views/registration.mjs
+++ views/registration.mjs
@@ -1,5 +1,55 @@
 "use strict"
 
-export function onUpdateGui(session) {
+import showAlert from "../js/pocket.mjs";
 
+export function onUpdateGui(session) {
+    session?.getGui?.buttonLeft0?.classList.remove('collapse');
+    session?.getGui?.buttonLeft1?.classList.add('collapse');
+    session?.getGui?.buttonRight0?.classList.add('collapse');
+    session?.getGui?.buttonRight1?.classList.add('collapse');
+    document.getElementById('form').addEventListener('submit', event => {
+        event.preventDefault();
+
+        let passwdInput = document.getElementById('passwdInput');
+        let confirmPasswdInput = document.getElementById('confirmPasswdInput');
+
+        let exit = false;
+        if(passwdInput?.value === '') {
+            const div =  document.getElementById('passwdInputError');
+            div.innerHTML = 'Password it\'s empty';
+            div.classList.remove('collapse');
+            exit = true;
+        }
+
+        if(confirmPasswdInput?.value === '') {
+            const div =  document.getElementById('confirmPasswdInputError');
+            div.innerHTML = 'Confirm password it\'s empty';
+            div.classList.remove('collapse');
+            exit = true;
+        }
+
+        if(exit) {
+            return;
+        }
+        
+        if(passwdInput?.value !== confirmPasswdInput?.value) {
+            showAlert('Passwords mismatch');
+            return;
+        } 
+
+        session.loadSynch('/');
+        
+
+
+
+
+      
+    });
+
+    let buttonLeftImage0 = session?.getGui?.buttonLeftImage0;
+    buttonLeftImage0.src = '/images/ic_back.svg';
+    buttonLeftImage0.addEventListener('click', () => 
+        session.loadSynch('/')
+    );
 }
+
